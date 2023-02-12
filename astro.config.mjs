a2348326a1e160ea0ec41b0c694db9b4158d3aec
Georges Haidar <ghaidar0@gmail.com>
2023-02-12 23:29:47 +0000

    auto-link headings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+import { autolinkConfig } from "./src/config/autolink";
 
 export default defineConfig({
 	site: "https://blog.disintegrator.dev",
@@ -16,5 +19,6 @@ export default defineConfig({
 	],
 	markdown: {
 		shikiConfig: { theme: "css-variables" },
+		rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, autolinkConfig]],
 	},
 });
